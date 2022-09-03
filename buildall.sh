614fadfa15b8a3f1296c177c74c216c48d761f62
Ian Klatzco <iklatzco@gmail.com>
2022-09-03 16:28:53 +0200

    buildall.sh comment; --legacy-peer-deps for old webpack version

--- buildall.sh
+++ buildall.sh
@@ -1,4 +1,4 @@
 cd automation; npm install; cd ..
 cd backend; npm install; cd ..
-cd extension; npm install; npm run build:dist; cd ..
+cd extension; npm install; npm run build:dist; cd .. # may need to use --legacy-peer-deps for old webpack version
 echo "Librevent is ready, not please follow https://libre.events/mobilizon-poster/"
